5011952ae96bad5e31331ec6d91b99418da42707
PrimeXaviers <mraldiafandi@gmail.com>
2020-04-29 19:07:01 +0700

    update

---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,4 @@
+<!-- Use Minified Plugins Version For Fast Page Load -->
+<script src="{{ asset('frontend/js/plugins.js') }}"></script>
+<script src="{{ asset('frontend/js/ajax-mail.js') }}"></script>
+<script src="{{ asset('frontend/js/custom.js') }}"></script>
