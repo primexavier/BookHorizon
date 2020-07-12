012cbfbda4f271390f04cd120031a68621a9cae6
primexaviers <mraldiafandi@gmail.com>
2020-07-12 19:25:34 +0700

    admin login update

---
+++ PXLaravel/resources/views/backend/auth/login.blade.php
@@ -0,0 +1,49 @@
+@extends('frontend.layouts.app')
+
+@section('content')
+    <main class="page-section inner-page-sec-padding-bottom">
+		<div class="container">
+            <div class="row justify-content-center">
+				<div class="col-sm-12 col-md-12 col-lg-10 col-xs-12">
+                    <form method="POST" action="{{ route('login.admin') }}">
+                        @csrf
+						<div class="login-form">
+							<h4 class="login-title">{{ __('Login') }}</h4>
+							<p><span class="font-weight-bold">I am a returning customer</span></p>
+							<div class="row">
+								<div class="col-md-12 col-12 mb--15">
+									<label for="email">{{ __('E-Mail Address') }}</label>
+                                    <input class="mb-0 form-control @error('email') is-invalid @enderror" id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+								</div>
+								<div class="col-12 mb--20">
+									<label for="password">{{ __('Password') }}</label>
+                                    <input class="mb-0 form-control @error('password') is-invalid @enderror" type="password" id="login-password" placeholder="Enter your password" name="password">
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+								<div class="col-md-12">
+                                    <button type="submit" class="btn btn-outlined">
+									    {{ __('Login') }}
+                                    </button>                                    
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</main>
+@endsection
---
+++ PXLaravel/resources/views/backend/auth/register.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+				<div class="login-form">
+					<h4 class="login-title">{{ __('Register') }}</h4>
+			    	<p><span class="font-weight-bold">I am a new customer</span></p>
+						<div class="row">
+							<div class="col-md-12 col-12 mb--15">
+								<label for="email">{{ __('Name') }}</label>
+								<input class="mb-0 form-control  @error('name') is-invalid @enderror" type="text" id="name" placeholder="Enter your full name" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+							<div class="col-12 mb--20">
+								<label for="email">{{ __('E-Mail Address') }}</label>
+                                <input class="mb-0 form-control @error('email') is-invalid @enderror" type="email" id="email" placeholder="Enter Your Email Address Here.." name="email" value="{{ old('email') }}" required autocomplete="email">
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+							</div>
+							<div class="col-lg-6 mb--20">
+								<label for="password">{{ __('Password') }}</label>
+								<input class="mb-0 form-control  @error('password') is-invalid @enderror" type="password" id="password" placeholder="Enter your password" name="password" value="{{ old('password') }}" required autocomplete="password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+							<div class="col-lg-6 mb--20">
+								<label for="password">{{ __('Confirm Password') }}</label>
+								<input class="mb-0 form-control" type="password" id="repeat-password" placeholder="Repeat your password" name="password_confirmation" required autocomplete="new-password">
+
+                            </div>
+							<div class="col-md-12">
+                                <button type="submit" class="btn btn-outlined">
+                                    {{ __('Register') }}
+                                </button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</form>
+        </div>
+    </div>
+</div>
+<br>
+@endsection
