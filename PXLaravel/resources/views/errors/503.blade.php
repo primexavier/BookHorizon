ca2f690b87e3fcf182d4d19c6a184ea66a3004dc
primexaviers <mraldiafandi@Gmail.com>
2020-08-07 04:04:09 +0700

    update style

--- PXLaravel/resources/views/errors/503.blade.php
+++ PXLaravel/resources/views/errors/503.blade.php
@@ -9,14 +9,15 @@
 <body>
 <!-- partial:index.partial.html -->
 <div class="container">
-  <h1>We Will Back Soon</h1>
-  <h1>{!! $exception->getMessage() !!}</h1>
-  <ul>
-    <li><span id="days"></span>days</li>
-    <li><span id="hours"></span>Hours</li>
-    <li><span id="minutes"></span>Minutes</li>
-    <li><span id="seconds"></span>Seconds</li>
-  </ul>
+    <image width="50%" src="frontend/image/logo.png"></image>
+    <h1>We Will Back Soon</h1>
+    <h1>{!! $exception->getMessage() !!}</h1>
+    <ul>
+        <li><span id="days"></span>days</li>
+        <li><span id="hours"></span>Hours</li>
+        <li><span id="minutes"></span>Minutes</li>
+        <li><span id="seconds"></span>Seconds</li>
+    </ul>
 </div>
 <!-- partial -->
   <script  src="countdown/script.js"></script>
