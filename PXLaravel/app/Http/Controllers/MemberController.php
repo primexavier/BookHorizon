dadd941bc55528784c0f4418a054bea45bcb7614
Aldi Afandi <mraldiafandi@gmail.com>
2020-09-09 15:56:45 +0700

    up

--- PXLaravel/app/Http/Controllers/MemberController.php
+++ PXLaravel/app/Http/Controllers/MemberController.php
@@ -264,7 +264,6 @@ class MemberController extends Controller
 
     public function pay(Request $request)
     {        
-        dd($request);
         $user_id = Auth::user()->id;
         $charts = Chart::where("user_id",$user_id)->get();
         if($charts->count() > 0){
