4438e0cd96c6e987e7e6568ed3eaa68ba43586e5
primexaviers <mraldiafandi@gmail.com>
2020-09-14 15:12:50 +0700

    update

--- PXLaravel/resources/views/index.blade.php
+++ PXLaravel/resources/views/index.blade.php
@@ -12,31 +12,31 @@
                                                                         "slidesToShow": 1,
                                                                         "dots":true
                                                                         }'>
-                                <div class="single-slide bg-image bg-overlay--white" data-bg="{{ asset('frontend/image/bg-images/home-4-slider-1.jpg') }}">
+                                <div class="single-slide bg-image bg-overlay--white" data-bg="{{ asset('frontend/image/bg-images/home-4-slider-1.png') }}">
                                     <div class="home-content text-left pl--30">
                                         <div class="row">
-                                            <div class="col-lg-5">
+                                            <!-- <div class="col-lg-5">
                                                 <span class="title-small">Magazine Cover</span>
                                                 <h1>Mockup.</h1>
                                                 <p>Cover up front of book and <br>leave summary</p>
                                                 <a href="{{route('index')}}" class="btn btn-outlined--pink">
                                                     Shop Now
                                                 </a>
-                                            </div>
+                                            </div> -->
                                         </div>
                                     </div>
                                 </div>
-                                <div class="single-slide bg-image bg-overlay--dark" data-bg="{{ asset('frontend/image/bg-images/home-4-slider-2.jpg') }}">
+                                <div class="single-slide bg-image bg-overlay--dark" data-bg="{{ asset('frontend/image/bg-images/home-4-slider-2.png') }}">
                                     <div class="home-content text-center">
                                         <div class="row justify-content-end">
-                                            <div class="col-lg-8">
+                                            <!-- <div class="col-lg-8">
                                                 <h1 class="v2">I Love This Idea!</h1>
                                                 <h2>Cover up front of book and
                                                     leave summary</h2>
                                                 <a href="{{route("index")}}" class="btn btn--yellow">
                                                     Shop Now
                                                 </a>
-                                            </div>
+                                            </div> -->
                                         </div>
                                     </div>
                                 </div>
